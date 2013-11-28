3ba276123866a58d7a1034e7d3b0a481da5ad707
betel <betelgeuse525@gmail.com>
2013-11-28 15:01:49 +0900

    セルを並べて表示しただけ

--- golgol/src/golgol/Cell.java
+++ golgol/src/golgol/Cell.java
@@ -4,17 +4,40 @@ import processing.core.PApplet;
 
 public class Cell {
 	PApplet p;
+
+	final int GREEN;
+	final int RED; // テスト用に、死んだセルは赤で表現する
+
 	int x, y; // セルの位置
 	int w; // セルの一辺の長さ
-	int color; // セルの色
-	boolean life; // セルの生死
+	boolean life; // セルの生死 (false=dead,true=alive)
 
 	Cell(PApplet p, int x, int y, int w) {
 		this.p = p;
 		this.x = x;
 		this.y = y;
 		this.w = w;
-		color = p.color(0, 200, 0); // 色は緑で初期化する
+		GREEN = p.color(0, 255, 0);
+		RED = p.color(255, 0, 0);
+	}
 
+	void drawCell() { // セルの描画
+		// translate()は使わない
+		if (life) { // セルの生死によって色を分ける
+			p.fill(GREEN);
+		} else {
+			p.fill(RED);
+		}
+
+		p.stroke(0); // グリッド線は黒色
+		p.rect(x, y, w, w); // 四角形を描く
+	}
+
+	void setBool(boolean life) { // セルの生死を決める
+		this.life = life;
+	}
+
+	boolean getBool() { // セルの生死を返す
+		return life;
 	}
 }
--- golgol/src/golgol/Main.java
+++ golgol/src/golgol/Main.java
@@ -5,15 +5,31 @@ import processing.core.PApplet;
 @SuppressWarnings("serial")
 public class Main extends PApplet {
 
-	final int W_WIDTH = 600; // ウィンドウサイズ
-	final int W_HEIGHT = 400;
+	static final int W_WINDOW = 600; // ウィンドウサイズ
+	static final int H_WINDOW = 400;
+	static final int W_CELL = 10; // セルのサイズ
+
+	Cell[][] cells;
+
+	int col, row;
 
 	public void setup() {
-		size(W_WIDTH, W_HEIGHT);
-
+		size(W_WINDOW, H_WINDOW); // ウィンドウサイズを指定
+		col = W_WINDOW / W_CELL; // ウィンドウに収まる分だけ配列を用意
+		row = H_WINDOW / W_CELL;
+		cells = new Cell[col][row];
+		for (int i = 0; i < col; i++) {
+			for (int j = 0; j < row; j++) {
+				cells[i][j] = new Cell(this, i * W_CELL, j * W_CELL, W_CELL);
+			}
+		}
 	}
 
 	public void draw() {
-
+		for (int i = 0; i < col; i++) {
+			for (int j = 0; j < row; j++) {
+				cells[i][j].drawCell();
+			}
+		}
 	}
 }
